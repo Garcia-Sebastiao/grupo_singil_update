0203f6616c403fda89f9d0705df8ca55ac9f7af7
Garcia-Sebastiao <garcia.sebastiao@statementmc.com>
2023-11-18 22:57:17 +0000

    bug fix

--- src/components/common/Carrousel/Carrousel.tsx
+++ src/components/common/Carrousel/Carrousel.tsx
@@ -1,5 +1,5 @@
 import { Swiper } from "swiper/react";
-import { Navigation, Autoplay } from "swiper/modules";
+import {  Autoplay } from "swiper/modules";
 
 import "swiper/css";
 import "./style.css";
